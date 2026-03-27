22cb2eb9d01c8e08db2650cbd60a619a8493fdbb
gilbertosantana <gilbertosantoss307@gmail.com>
2026-03-27 15:04:58 -0300

    BoardException and defensive programming

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -7,7 +7,7 @@ import chess.pieces.Rook;
 
 public class ChessMatch {
 
-    private Board board;
+    private final Board board;
 
     public  ChessMatch() {
         this.board = new Board(8, 8);
