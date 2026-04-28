4dc76fc8532a7f9b29d8a587eb8517b61ea497b5
gilbertosantana <gilbertosantoss307@gmail.com>
2026-04-28 16:54:47 -0300

    Possible moves of a piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -44,6 +44,10 @@ public class ChessMatch {
         if (!board.thereIsAPiece(position)) {
             throw new ChessException("There is no piece on source position.");
         }
+
+        if(!board.piece(position).isThereAnypossibleMove()) {
+            throw new ChessException("There is no possible moves for the chosen piece");
+        }
     }
 
     public void placeNewPiece(char column, int row, ChessPiece piece) {
